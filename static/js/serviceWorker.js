b96c663262fa2d5c7576a242e3b6bb58998c9267
Rejone Hossen <iamrejonehossen@gmail.com>
2025-06-18 22:53:25 +0600

    serviceWorker.js create:

---
+++ static/js/serviceWorker.js
@@ -0,0 +1,81 @@
+// Created by Rejone Hossen | Premium Task Manager | 2025
+
+const CACHE_NAME = 'premium-task-manager-v1';
+const urlsToCache = [
+  '/',
+  '/static/css/styles.css',
+  '/static/js/main.js',
+  '/static/js/dragdrop.js',
+  '/static/images/logo.png',
+  '/offline/'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        
+        // Clone the request
+        const fetchRequest = event.request.clone();
+        
+        return fetch(fetchRequest).then(
+          response => {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            // Clone the response
+            const responseToCache = response.clone();
+            
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+              
+            return response;
+          }
+        ).catch(() => {
+          // If both fetch and cache fail, show offline page
+          if (event.request.headers.get('accept').includes('text/html')) {
+            return caches.match('/offline/');
+          }
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
+
+
+
+
+
