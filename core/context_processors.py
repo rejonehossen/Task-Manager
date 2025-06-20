1c5fdb74e539c80cbe6f0b63cb1ef4efe473d6d8
Rejone Hossen <iamrejonehossen@gmail.com>
2025-06-20 17:50:28 +0600

    share list hide

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,5 +1,14 @@
 # Created by Rejone Hossen | Premium Task Manager | 2025
-
+from .models import SharedList
 def dark_mode(request):
     dark_mode = request.COOKIES.get('darkMode', 'false') == 'true'
     return {'dark_mode': dark_mode}
+
+
+def shared_list(request):
+    my_list = shared_list.objects.all()
+    return (
+        {
+            'shared_list':my_list,
+        }
+    )
